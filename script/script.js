9bd5e89bcb4559d12e4a5448cdbf74d831372a43
neox56 <neox56@gmail.com>
2020-01-17 13:44:15 +0400

    1.Реализован вывод товаров на страницу 2.Реализована фильтрация товаров по категориям 3.Реализован прелоадер загрузки

--- script/script.js
+++ script/script.js
@@ -6,9 +6,25 @@ document.addEventListener('DOMContentLoaded', function () {
     const wishlistBtn = document.getElementById('wishlist');
     const goodsWrapper = document.querySelector('.goods-wrapper');
     const cart = document.querySelector('.cart');
-
+    const category = document.querySelector('.category');
+    const preloader = document.querySelector('.preloader');
 
     //Functions
+
+    //ф-ция показывает прелоадер
+    const showSpinner = () => {
+        document.querySelector('main').style.display = 'none';
+        preloader.style.display = 'flex';
+    }
+
+    //ф-ция скрывает прелоадер
+    const hideSpinner = () => {
+        setTimeout(() => {
+            document.querySelector('main').style.display = 'block';
+            preloader.style.display = 'none';
+        }, 2000)
+    }
+
     //ф-ция создает карточки товаров
     const createCardGoods = (id, title, price, img) => {
         const card = document.createElement('div');
@@ -28,18 +44,13 @@ document.addEventListener('DOMContentLoaded', function () {
         </div>`;
         return card;
     }
-    goodsWrapper.append(createCardGoods(1, 'Дартс', 2000, 'img/temp/Archer.jpg'));
-    goodsWrapper.append(createCardGoods(2, 'Фламинго', 3000, 'img/temp/Flamingo.jpg'));
-    goodsWrapper.append(createCardGoods(3, 'Носки', 333, 'img/temp/Socks.jpg'));
+
 
     //ф-ция открывает корзину
     const openCart = (e) => {
-
-        if (e.target.classList.contains('card-title')) {
-            e.preventDefault();
-            return;
-        }
+        e.preventDefault();
         cart.style.display = 'flex';
+        document.body.addEventListener('keydown', closeCart);
     }
 
     //ф-ция закрывает корзину
@@ -47,14 +58,76 @@ document.addEventListener('DOMContentLoaded', function () {
         target,
         key
     }) => {
-
-        if (target === cart || target.classList.contains('cart-close') || key === 'Escape')
+        if (target === cart || target.classList.contains('cart-close') || key === 'Escape') {
             cart.style.display = '';
+            document.body.removeEventListener('keydown', closeCart);
+        }
     }
+
+    //ф-ция выводит товары на страницу
+    const renderCard = items => {
+
+        clearGoods();
+
+        hideSpinner();
+
+        items.forEach(({
+            id,
+            title,
+            price,
+            imgMin
+        } = {}) => {
+            goodsWrapper.append(createCardGoods(id, title, price, imgMin));
+        });
+    }
+
+    //ф-ция очищает товары
+    const clearGoods = () => {
+        //goodsWrapper.textContent = '';
+
+        let child = goodsWrapper.lastElementChild;
+        while (child) {
+            child.remove();
+            child = goodsWrapper.lastElementChild;
+        }
+    }
+
+    //ф-ция получает товары
+    const getGoods = (handler, filter) => {
+
+        showSpinner();
+
+        fetch('db/db.json')
+            .then(response => response.json())
+            .then(filter)
+            .then(handler)
+    }
+
+    //ф-ция фильтрует по категориям
+    const chooseCategory = (e) => {
+        e.preventDefault();
+        const target = e.target;
+
+        if (target.classList.contains('category-item')) {
+            const category = target.dataset.category;
+
+            getGoods(renderCard, (goods) => {
+                return newGoods = goods.filter(item => item.category.includes(category));
+            });
+        }
+    };
+
+    //ф-ция сортирует товары в случайном порядке
+    const randomSort = item => {
+        return item.sort(() => Math.random() - 0.5);
+    }
+
+
+    getGoods(renderCard, randomSort);
 
     //Events
     cartBtn.addEventListener('click', openCart);
     cart.addEventListener('click', closeCart);
-    document.body.addEventListener('keydown', closeCart);
-    document.querySelectorAll('.card-title').forEach(link => link.addEventListener('click', openCart));
+    category.addEventListener('click', chooseCategory)
+
 });
